3f220a9a6afce775f2c5b4f1de52d6d0c8a4bfac
RaindropsZhen <wuzhenze@hotmail.com>
2025-01-02 13:30:37 -0100

    [checkpoint]

--- authUser/apps.py
+++ authUser/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
-
+from django.conf import settings
+import os 
 
 class AuthuserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authUser'
-    path = '/data/new_backend/authUser'
+    path = os.path.join(settings.BASE_DIR, 'authUser')
--- core/apps.py
+++ core/apps.py
@@ -1,7 +1,9 @@
 from django.apps import AppConfig
 
+import os
+from django.conf import settings
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-    path = '/data/new_backend/core'
+    path = os.path.join(settings.BASE_DIR, 'core')
