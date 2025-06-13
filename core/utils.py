2752bbd63911592c9317c8df5c2fab7db1bae39c
RaindropsZhen <34010629+RaindropsZhen@users.noreply.github.com>
2025-06-13 16:56:20 +0100

    fix: deduplicate printed name

--- core/utils.py
+++ core/utils.py
@@ -6,11 +6,6 @@ from django.utils import timezone
 from googletrans import Translator
 from collections import defaultdict
 import ast
-# SUSHI 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119
-# DRINKS 188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315,316,322,323,324,325,326,327,328,329,330,331
-# Desserts 172,173,174,175,176,177,178
-# Chinese cuisine 140,141,142,143,144,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164
-# 120,121,122,123,124,125,126,128,129,136,137,138,139,145,146,147
 
 def generate_signature(user, user_key, timestamp):
     # Concatenate user, user_key, and timestamp
@@ -87,7 +82,7 @@ def format_list_as_string(name_quantity_list,font_size):
 <L><LINE p="23" /><B>菜名</B><HT><B>数量</B><BR>
 --------------------------------<BR>"""
         for item in name_quantity_list:
-            name_to_print = item['name'].split('.')[0] + item['name_to_print']
+            name_to_print = item['name_to_print'] # Use the already prepared name_to_print
             formatted_string += f"<B2>{name_to_print}<HT>{item['quantity']}<BR></B2><BR>"
             
         formatted_string += """--------------------------------<BR>
@@ -97,7 +92,7 @@ def format_list_as_string(name_quantity_list,font_size):
 <L><LINE p="23" /><B>菜名</B><HT><B>数量</B><BR>
 --------------------------------<BR>"""
         for item in name_quantity_list:
-            name_to_print = item['name'].split('.')[0] + item['name_to_print']
+            name_to_print = item['name_to_print'] # Use the already prepared name_to_print
             formatted_string += f"<B>{name_to_print}<HT>{item['quantity']}<BR></B><BR>"
             
         formatted_string += """--------------------------------<BR>
